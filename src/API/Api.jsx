26ea29c4f03d45d0d14631ed9d488c91d29a4686
Dipanshu <dipanshu.patel.wwl@gmail.com>
2025-07-09 14:00:46 +0530

    change base url

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gwl-back-1.onrender.com/api",
+  baseURL: "https://gwl-back-1.onrender.com/api" || "http://localhost:4000/api",
   withCredentials: true,
 })
 export const BASE_URL = "http://localhost:4000";
