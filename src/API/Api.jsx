e6c871bb7898d47e76ee883db4e4ceaba976b03a
WorldWeblogic <shantanu.kr.worldweblogic@gmail.com>
2025-08-26 18:15:39 +0530

    update url

--- src/API/Api.jsx
+++ src/API/Api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL:'https://gwl-back-lcoa.onrender.com/api',
+  // baseURL: "http://localhost:4000/api",
+  baseURL:'https://gwl-back-lcoa.onrender.com/api',
   withCredentials: true,
 })
-// export const BASE_URL='https://gwl-back-lcoa.onrender.com';
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL='https://gwl-back-lcoa.onrender.com';
+// export const BASE_URL = "http://localhost:4000";
 export default API;
